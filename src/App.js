781efee243b710e1a922d2cfa59f7e40d754623d
furkantuna <tunaf941@gmail.com>
2023-05-13 20:11:34 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { usePosition } from "use-position";
+import axios from "axios";
 function App() {
+  const [weather, setWeather] = useState();
+  const { latitude, longitude } = usePosition();
+  const key = process.env.REACT_APP_WEATHER_DATA;
+  const [loading, setLoading] = useState(false);
+
+  const getWeatherData = async (lat, lon) => {
+    const lang = navigator.language.split("_")[0];
+    setLoading(true);
+    try {
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&lang=${lang}`
+      );
+      console.log(data);
+      setWeather(data);
+      setLoading(false);
+    } catch {
+      setLoading(false);
+      console.error("veriler çekilemedi.");
+    }
+  };
+
+  useEffect(() => {
+    latitude && longitude && getWeatherData(latitude, longitude);
+  }, [latitude, longitude]);
+
+  if (loading) return <div>Loading</div>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h2>Hava Durumu</h2>
+      <h3>Enlem Kordinat:{latitude}</h3>
+      <h3>Boylam Kordinat:{longitude}</h3>
+      <h3>Kordinat Bölgesi:{weather?.name}</h3>
+      <h3>Hava Sıcaklığı:{Math.ceil(weather?.main.temp - 273.15)}</h3>
+      <h3>Durumu;{weather?.weather.map((data) => data.main)}</h3>
+      <h3>Özelliği:{weather?.weather.map((data) => data.description)}</h3>
     </div>
   );
 }
-
 export default App;
